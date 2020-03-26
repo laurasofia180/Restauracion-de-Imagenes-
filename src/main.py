350beb30d69c01ca9d172127b5f6d2c293887cce
Laura Sofia Muñoz Montoya <lmunozm@eafit.edu.co>
2020-03-26 02:27:51 -0500

    Laplace funciona

--- src/main.py
+++ src/main.py
@@ -4,84 +4,95 @@ import random
 import numpy as np
 
 
-def interpolarLaplace(oprarray, imagearray):
-    for pixelY in range(len(oprarray)):
-        for pixelX in range(len(oprarray[0])):
-            if oprarray[i, j] == 99999:
+def main():
+    # Leer imagen - Blanco y negro para un solo canal de color.
+    image = Image.open('./imagenes/cat.jpg').convert('RGB').convert('LA')
+
+    value = [int(x / 2) for x in image.size]
+    image = image.resize(value)
+
+    image_array = np.array(image)
+    bad_array = image_array.copy()
+    # Daño el array
+    for i in range(len(bad_array)):
+        for j in range(len(bad_array[0])):
+            if random.random() < 0.5:
+                # Pixel malo
+                bad_array[i, j] = [255, 255]
+    bad_array_copy = bad_array.copy()
+    # Proceso
+    cols = len(bad_array)
+    rows = len(bad_array[0])
+    matriz_operacional = np.empty((cols, rows))
+
+    # Creo una matriz para operaciones
+    for i in range(len(bad_array)):
+        for j in range(len(bad_array[0])):
+            if bad_array[i, j, 0] > 254:
+                matriz_operacional[i, j] = int(0)
+            else:
+                matriz_operacional[i, j] = int(bad_array[i, j, 0])
+
+    # Laplace
+    for pixelY in range(len(matriz_operacional)):
+        for pixelX in range(len(matriz_operacional[0])):
+            if matriz_operacional[pixelY, pixelX] == 0:
                 if pixelY == 0 and pixelX == 0:
                     solved = (
-                        np.linalg.solve([[1]], [(oprarray[pixelY, pixelX + 1] + oprarray[pixelY + 1, pixelX]) * 0.5]))
-                    imagearray[i, j, 0] = int(solved[0])
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY, pixelX + 1] + matriz_operacional[pixelY + 1, pixelX]) * 0.5]))
+                    bad_array[pixelY, pixelX, 0] = int(solved[0])
                     continue
-                if pixelY == 0 and pixelX == len(oprarray[0]) - 1:
+                if pixelY == 0 and pixelX == len(matriz_operacional[0]) - 1:
                     solved = (
-                        np.linalg.solve([[1]], [(oprarray[pixelY, pixelX - 1] + oprarray[pixelY + 1, pixelX]) * 0.5]))
-                    imagearray[i, j, 0] = int(solved[0])
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY, pixelX - 1] + matriz_operacional[pixelY + 1, pixelX]) * 0.5]))
+                    bad_array[pixelY, pixelX, 0] = int(solved[0])
                     continue
-                if pixelY == len(oprarray) - 1 and pixelX == 0:
+                if pixelY == len(matriz_operacional) - 1 and pixelX == 0:
                     solved = (
-                        np.linalg.solve([[1]], [(oprarray[pixelY, pixelX + 1] + oprarray[pixelY - 1, pixelX]) * 0.5]))
-                    imagearray[i, j, 0] = int(solved[0])
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY, pixelX + 1] + matriz_operacional[pixelY - 1, pixelX]) * 0.5]))
+                    bad_array[pixelY, pixelX, 0] = int(solved[0])
                     continue
-                if pixelY == len(oprarray) - 1 and pixelX == len(oprarray[0]) - 1:
+                if pixelY == len(matriz_operacional) - 1 and pixelX == len(matriz_operacional[0]) - 1:
                     solved = (
-                        np.linalg.solve([[1]], [(oprarray[pixelY - 1, pixelX] + oprarray[pixelY - 1, pixelX]) * 0.5]))
-                    imagearray[i, j, 0] = int(solved[0])
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY - 1, pixelX] + matriz_operacional[pixelY - 1, pixelX]) * 0.5]))
+                    bad_array[pixelY, pixelX, 0] = int(solved[0])
                     continue
-                if pixelY == 0 or pixelY == len(oprarray) - 1:
+                if pixelY == 0 or pixelY == len(matriz_operacional) - 1:
                     solved = (
-                        np.linalg.solve([[1]], [(oprarray[pixelY, pixelX - 1] + oprarray[pixelY, pixelX + 1]) * 0.5]))
-                    imagearray[i, j, 0] = int(solved[0])
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY, pixelX - 1] + matriz_operacional[pixelY, pixelX + 1]) * 0.5]))
+                    bad_array[pixelY, pixelX, 0] = int(solved[0])
                     continue
-                if pixelX == 0 or pixelX == len(oprarray[0]) - 1:
+                if pixelX == 0 or pixelX == len(matriz_operacional[0]) - 1:
                     solved = (
-                        np.linalg.solve([[1]], [(oprarray[pixelY - 1, pixelX] + oprarray[pixelY + 1, pixelX]) * 0.5]))
-                    imagearray[i, j, 0] = int(solved[0])
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY - 1, pixelX] + matriz_operacional[pixelY + 1, pixelX]) * 0.5]))
+                    bad_array[pixelY, pixelX, 0] = int(solved[0])
                     continue
                     # Punto normal
                 solved = (np.linalg.solve([[1]], [
-                    (oprarray[pixelY + 1, pixelX] + oprarray[pixelY - 1, pixelX] + oprarray[pixelY, pixelX + 1] +
-                     oprarray[
+                    (matriz_operacional[pixelY + 1, pixelX] + matriz_operacional[pixelY - 1, pixelX] +
+                     matriz_operacional[pixelY, pixelX + 1] +
+                     matriz_operacional[
                          pixelY, pixelX - 1]) * 0.25]))
-                imagearray[i, j, 0] = int(solved[0])
+                bad_array[pixelY, pixelX, 0] = int(solved[0])
             else:
-                imagearray[i, j, 0] = oprarray[i, j]
-    return imagearray
+                bad_array[pixelY, pixelX, 0] = matriz_operacional[pixelY, pixelX]
+
+    # Graficar
+    imgplot, axarr = plt.subplots(3)
+    axarr[0].imshow(Image.fromarray(image_array))
+    axarr[0].set_title("Original")
+    axarr[1].imshow(Image.fromarray(bad_array_copy))
+    axarr[1].set_title("Dañada")
+    axarr[2].imshow(Image.fromarray(bad_array))
+    axarr[2].set_title("Interpolada con Laplace")
+    plt.show()
 
 
-# Cargar imagen
-f = Image.open('./imagenes/cat.jpg').convert('RGB').convert('LA')
-value = [int(x / 2) for x in f.size]
-f = f.resize(value)
-x, y = f.size
-# Convertir en array
-arrayoriginal = np.array(f)
-arraycopia = arrayoriginal.copy()
-array2 = np.full((y, x), 0)
-
-for i in range(len(arrayoriginal)):
-    for j in range(len(arrayoriginal[0])):
-        if random.random() < 0.5:
-            newValue = arrayoriginal[i, j]
-            newValue[0] = 255
-            arrayoriginal[i, j] = newValue
-
-for i in range(len(array2)):
-    for j in range(len(array2[0])):
-        if arrayoriginal[i, j, 0] == 255:
-            array2[i, j] = 99999
-        else:
-            array2[i, j] = arrayoriginal[i, j, 0]
-
-interpolado = interpolarLaplace(array2, arrayoriginal)
-
-imagenoriginal = Image.fromarray(arraycopia)
-imagendanada = Image.fromarray(arrayoriginal)
-imageninterpolada = Image.fromarray(interpolado)
-
-f, axarr = plt.subplots(3)
-axarr[0].imshow(imagenoriginal)
-axarr[1].imshow(imagendanada)
-axarr[2].imshow(imageninterpolada)
-
-plt.show()
+if __name__ == '__main__':
+    main()
