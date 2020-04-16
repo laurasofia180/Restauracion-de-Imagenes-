4b6972535049bc23e968a4c7f996ed40acbc7d7e
Laura Sofia Muñoz Montoya <lmunozm@eafit.edu.co>
2020-04-15 23:35:56 -0500

    Interpolacion con RGB

--- src/main.py
+++ src/main.py
@@ -4,9 +4,146 @@ import random
 import numpy as np
 
 
+def interpolar(matriz_operacional, bad_array):
+    for pixelY in range(len(matriz_operacional)):
+        for pixelX in range(len(matriz_operacional[0])):
+            if matriz_operacional[pixelY, pixelX, 0] == 0:
+                if pixelY == 0 and pixelX == 0:
+                    # solved = (
+                    #     np.linalg.solve([[1]], [
+                    #         (matriz_operacional[pixelY, pixelX + 1] + matriz_operacional[pixelY + 1, pixelX]) * 0.5]))
+                    solvedR = (np.linalg.solve([[1]], [
+                        (matriz_operacional[pixelY, pixelX + 1, 0] + matriz_operacional[pixelY + 1, pixelX, 0]) * 0.5]))
+
+                    solvedG = (np.linalg.solve([[1]], [
+                        (matriz_operacional[pixelY, pixelX + 1, 1] + matriz_operacional[pixelY + 1, pixelX, 1]) * 0.5]))
+                    solvedB = (np.linalg.solve([[1]], [
+                        (matriz_operacional[pixelY, pixelX + 1, 2] + matriz_operacional[pixelY + 1, pixelX, 2]) * 0.5]))
+
+                    bad_array[pixelY, pixelX] = [int(solvedR[0]), int(solvedB[0]), int(solvedG[0])]
+                    continue
+                if pixelY == 0 and pixelX == len(matriz_operacional[0]) - 1:
+                    # solved = (
+                    #     np.linalg.solve([[1]], [
+                    #         (matriz_operacional[pixelY, pixelX - 1] + matriz_operacional[pixelY + 1, pixelX]) * 0.5]))
+                    solvedR = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY, pixelX - 1, 0] + matriz_operacional[
+                                pixelY + 1, pixelX, 0]) * 0.5]))
+                    solvedG = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY, pixelX - 1, 1] + matriz_operacional[
+                                pixelY + 1, pixelX, 1]) * 0.5]))
+                    solvedB = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY, pixelX - 1, 2] + matriz_operacional[
+                                pixelY + 1, pixelX, 2]) * 0.5]))
+
+                    bad_array[pixelY, pixelX] = [int(solvedR[0]), int(solvedB[0]), int(solvedG[0])]
+                    continue
+                if pixelY == len(matriz_operacional) - 1 and pixelX == 0:
+                    # solved = (
+                    #     np.linalg.solve([[1]], [
+                    #         (matriz_operacional[pixelY, pixelX + 1] + matriz_operacional[pixelY - 1, pixelX]) * 0.5]))
+                    solvedR = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY, pixelX + 1, 0] + matriz_operacional[
+                                pixelY - 1, pixelX, 0]) * 0.5]))
+                    solvedG = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY, pixelX + 1, 1] + matriz_operacional[
+                                pixelY - 1, pixelX, 1]) * 0.5]))
+                    solvedB = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY, pixelX + 1, 2] + matriz_operacional[
+                                pixelY - 1, pixelX, 2]) * 0.5]))
+
+                    bad_array[pixelY, pixelX] = [int(solvedR[0]), int(solvedB[0]), int(solvedG[0])]
+                    continue
+                if pixelY == len(matriz_operacional) - 1 and pixelX == len(matriz_operacional[0]) - 1:
+                    # solved = (
+                    #     np.linalg.solve([[1]], [
+                    #         (matriz_operacional[pixelY - 1, pixelX] + matriz_operacional[pixelY - 1, pixelX]) * 0.5]))
+                    solvedR = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY - 1, pixelX, 0] + matriz_operacional[
+                                pixelY - 1, pixelX, 0]) * 0.5]))
+                    solvedG = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY - 1, pixelX, 1] + matriz_operacional[
+                                pixelY - 1, pixelX, 1]) * 0.5]))
+                    solvedB = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY - 1, pixelX, 2] + matriz_operacional[
+                                pixelY - 1, pixelX, 2]) * 0.5]))
+                    bad_array[pixelY, pixelX] = [int(solvedR[0]), int(solvedB[0]), int(solvedG[0])]
+                    continue
+                if pixelY == 0 or pixelY == len(matriz_operacional) - 1:
+                    # solved = (
+                    #     np.linalg.solve([[1]], [
+                    #         (matriz_operacional[pixelY, pixelX - 1] + matriz_operacional[pixelY, pixelX + 1]) * 0.5]))
+                    solvedR = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY, pixelX - 1, 0] + matriz_operacional[
+                                pixelY, pixelX + 1, 0]) * 0.5]))
+                    solvedG = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY, pixelX - 1, 1] + matriz_operacional[
+                                pixelY, pixelX + 1, 1]) * 0.5]))
+                    solvedB = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY, pixelX - 1, 2] + matriz_operacional[
+                                pixelY, pixelX + 1, 2]) * 0.5]))
+                    bad_array[pixelY, pixelX] = [int(solvedR[0]), int(solvedB[0]), int(solvedG[0])]
+                    continue
+                if pixelX == 0 or pixelX == len(matriz_operacional[0]) - 1:
+                    # solved = (
+                    #     np.linalg.solve([[1]], [
+                    #         (matriz_operacional[pixelY - 1, pixelX] + matriz_operacional[pixelY + 1, pixelX]) * 0.5]))
+                    solvedR = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY - 1, pixelX, 0] + matriz_operacional[
+                                pixelY + 1, pixelX, 0]) * 0.5]))
+                    solvedG = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY - 1, pixelX, 1] + matriz_operacional[
+                                pixelY + 1, pixelX, 1]) * 0.5]))
+                    solvedB = (
+                        np.linalg.solve([[1]], [
+                            (matriz_operacional[pixelY - 1, pixelX, 2] + matriz_operacional[
+                                pixelY + 1, pixelX, 2]) * 0.5]))
+
+                    bad_array[pixelY, pixelX] = [int(solvedR[0]), int(solvedB[0]), int(solvedG[0])]
+                    continue
+                    # Punto normal
+                # solved = (np.linalg.solve([[1]], [
+                #     (matriz_operacional[pixelY + 1, pixelX] + matriz_operacional[pixelY - 1, pixelX] +
+                #      matriz_operacional[pixelY, pixelX + 1] +
+                #      matriz_operacional[
+                #          pixelY, pixelX - 1]) * 0.25]))
+                solvedR = (np.linalg.solve([[1]], [
+                    (matriz_operacional[pixelY + 1, pixelX, 0] + matriz_operacional[pixelY - 1, pixelX, 0] +
+                     matriz_operacional[pixelY, pixelX + 1, 0] +
+                     matriz_operacional[
+                         pixelY, pixelX - 1, 0]) * 0.25]))
+                solvedG = (np.linalg.solve([[1]], [
+                    (matriz_operacional[pixelY + 1, pixelX, 1] + matriz_operacional[pixelY - 1, pixelX, 1] +
+                     matriz_operacional[pixelY, pixelX + 1, 1] +
+                     matriz_operacional[
+                         pixelY, pixelX - 1, 1]) * 0.25]))
+                solvedB = (np.linalg.solve([[1]], [
+                    (matriz_operacional[pixelY + 1, pixelX, 2] + matriz_operacional[pixelY - 1, pixelX, 2] +
+                     matriz_operacional[pixelY, pixelX + 1, 2] +
+                     matriz_operacional[
+                         pixelY, pixelX - 1, 2]) * 0.25]))
+                bad_array[pixelY, pixelX] = [int(solvedR[0]), int(solvedB[0]), int(solvedG[0])]
+            else:
+                bad_array[pixelY, pixelX] = matriz_operacional[pixelY, pixelX]
+
+
 def main():
     # Leer imagen - Blanco y negro para un solo canal de color.
-    image = Image.open('./imagenes/cat.jpg').convert('RGB').convert('LA')
+    image = Image.open('./imagenes/1280x720.jpg').convert('RGB')
 
     value = [int(x / 2) for x in image.size]
     image = image.resize(value)
@@ -18,70 +155,23 @@ def main():
         for j in range(len(bad_array[0])):
             if random.random() < 0.5:
                 # Pixel malo
-                bad_array[i, j] = [255, 255]
+                bad_array[i, j] = [255, 255, 255]
     bad_array_copy = bad_array.copy()
     # Proceso
     cols = len(bad_array)
     rows = len(bad_array[0])
-    matriz_operacional = np.empty((cols, rows))
+    matriz_operacional = np.empty((cols, rows, 3))
 
     # Creo una matriz para operaciones
     for i in range(len(bad_array)):
         for j in range(len(bad_array[0])):
             if bad_array[i, j, 0] > 254:
-                matriz_operacional[i, j] = int(0)
+                matriz_operacional[i, j] = [0, 0, 0]
             else:
-                matriz_operacional[i, j] = int(bad_array[i, j, 0])
+                matriz_operacional[i, j] = bad_array[i, j]
 
     # Laplace
-    for pixelY in range(len(matriz_operacional)):
-        for pixelX in range(len(matriz_operacional[0])):
-            if matriz_operacional[pixelY, pixelX] == 0:
-                if pixelY == 0 and pixelX == 0:
-                    solved = (
-                        np.linalg.solve([[1]], [
-                            (matriz_operacional[pixelY, pixelX + 1] + matriz_operacional[pixelY + 1, pixelX]) * 0.5]))
-                    bad_array[pixelY, pixelX, 0] = int(solved[0])
-                    continue
-                if pixelY == 0 and pixelX == len(matriz_operacional[0]) - 1:
-                    solved = (
-                        np.linalg.solve([[1]], [
-                            (matriz_operacional[pixelY, pixelX - 1] + matriz_operacional[pixelY + 1, pixelX]) * 0.5]))
-                    bad_array[pixelY, pixelX, 0] = int(solved[0])
-                    continue
-                if pixelY == len(matriz_operacional) - 1 and pixelX == 0:
-                    solved = (
-                        np.linalg.solve([[1]], [
-                            (matriz_operacional[pixelY, pixelX + 1] + matriz_operacional[pixelY - 1, pixelX]) * 0.5]))
-                    bad_array[pixelY, pixelX, 0] = int(solved[0])
-                    continue
-                if pixelY == len(matriz_operacional) - 1 and pixelX == len(matriz_operacional[0]) - 1:
-                    solved = (
-                        np.linalg.solve([[1]], [
-                            (matriz_operacional[pixelY - 1, pixelX] + matriz_operacional[pixelY - 1, pixelX]) * 0.5]))
-                    bad_array[pixelY, pixelX, 0] = int(solved[0])
-                    continue
-                if pixelY == 0 or pixelY == len(matriz_operacional) - 1:
-                    solved = (
-                        np.linalg.solve([[1]], [
-                            (matriz_operacional[pixelY, pixelX - 1] + matriz_operacional[pixelY, pixelX + 1]) * 0.5]))
-                    bad_array[pixelY, pixelX, 0] = int(solved[0])
-                    continue
-                if pixelX == 0 or pixelX == len(matriz_operacional[0]) - 1:
-                    solved = (
-                        np.linalg.solve([[1]], [
-                            (matriz_operacional[pixelY - 1, pixelX] + matriz_operacional[pixelY + 1, pixelX]) * 0.5]))
-                    bad_array[pixelY, pixelX, 0] = int(solved[0])
-                    continue
-                    # Punto normal
-                solved = (np.linalg.solve([[1]], [
-                    (matriz_operacional[pixelY + 1, pixelX] + matriz_operacional[pixelY - 1, pixelX] +
-                     matriz_operacional[pixelY, pixelX + 1] +
-                     matriz_operacional[
-                         pixelY, pixelX - 1]) * 0.25]))
-                bad_array[pixelY, pixelX, 0] = int(solved[0])
-            else:
-                bad_array[pixelY, pixelX, 0] = matriz_operacional[pixelY, pixelX]
+    interpolar(matriz_operacional, bad_array)
 
     # Graficar
     imgplot, axarr = plt.subplots(3)
